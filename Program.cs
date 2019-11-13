59ccb7836ea60ab6535dcc9b188ff25baaddbf17
Dennis Kafton <boom1axe@att.net>
2019-11-13 13:24:22 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TriviaAPIConsumer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Retriving trivia question....");
+
+            TriviaClient trivia = new TriviaClient();
+            string result = await trivia.GetTriviaQuestionsAsync();
+
+            Console.WriteLine("Resceived trivia question");
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TriviaClient.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaAPIConsumer
+{
+    public class TriviaClient
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        //Static constructor; runs once per class
+        //https://docs.microsoft.com/en-us/dotnet/api/system.net.http.httpclient?view=netframework-4.8
+        static TriviaClient()
+        {
+            //Initialize all HttpClient settings
+            client.BaseAddress = new Uri("https://opendb.com/");
+            
+        }
+
+        public async Task<string> GetTriviaQuestionsAsync()
+        {
+            HttpResponseMessage response = 
+                await client.GetAsync("api.php?amount=5");
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                return data;
+            }
+            else
+            {
+                //if not successful, null is returned
+                return null;
+            }
+        }
+    }
+
+    public class Result
+    {
+        public string category { get; set; }
+        public string type { get; set; }
+        public string difficulty { get; set; }
+        public string question { get; set; }
+        public string correct_answer { get; set; }
+        public List<string> incorrect_answers { get; set; }
+    }
+
+    public class TriviaResponse
+    {
+        public int response_code { get; set; }
+        public List<Result> results { get; set; }
+    }
+
+}
